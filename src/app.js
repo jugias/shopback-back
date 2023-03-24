c84d5859267bb0605fb6a354bd6974600d898ce7
M0n00s <programacion.buffalo@gmail.com>
2023-03-24 11:14:21 -0400

    update coors policy

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://frontend-g-10.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://shopbag-g10.onrender.com"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
