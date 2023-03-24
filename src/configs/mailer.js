ad7e9ab0df233af7e75c376427a6e28bdbe68ded
M0n00s <programacion.buffalo@gmail.com>
2023-03-24 08:50:56 -0400

    node mailer config done

--- src/configs/mailer.js
+++ src/configs/mailer.js
@@ -1,18 +1,16 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
- const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, 
-    auth: {
-      user: "efdwxstkn5itdeg6@ethereal.email", 
-      pass: "ucwbfQRVgf2D1pEDC3", 
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "garrison.cremin4@ethereal.email",
+    pass: "6TgDWjd3wseuZvvSsw",
+  },
+});
 transporter.verify().then(() => {
-  console.log('Server is ready for send emails');
-})
-
-
+  console.log("Server is ready for send emails");
+});
 
 module.exports = transporter;
